e9dcbc0ca6953c63680e1e159341d158e13c859f
AppleFlavored <boostelixir@gmail.com>
2022-05-23 21:10:25 -0400

    Start on NodeCanvas

--- src/main/java/io/github/dasheditor/gui/DashApplication.java
+++ src/main/java/io/github/dasheditor/gui/DashApplication.java
@@ -1,8 +1,11 @@
 package io.github.dasheditor.gui;
 
+import io.github.dasheditor.gui.node.NodeCanvas;
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.control.Label;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class DashApplication extends Application {
@@ -10,6 +13,9 @@ public class DashApplication extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         final BorderPane root = new BorderPane();
+
+        final NodeCanvas canvas = new NodeCanvas();
+        root.setCenter(canvas);
 
         stage.setTitle("Dash");
         stage.setScene(new Scene(root, 900, 600));

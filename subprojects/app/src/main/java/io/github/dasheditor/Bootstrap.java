abc436e68bcfac956cb846fefd3c7b51491c3206
AppleFlavored <boostelixir@gmail.com>
2023-07-20 16:18:48 -0400

    Enable hardware acceleration for host platform

--- subprojects/app/src/main/java/io/github/dasheditor/Bootstrap.java
+++ subprojects/app/src/main/java/io/github/dasheditor/Bootstrap.java
@@ -5,9 +5,17 @@ import io.github.dasheditor.ui.EditorWindow;
 public class Bootstrap {
 
     public static void main(String[] args) {
-        System.setProperty("sun.java2d.opengl", "true");
+        // Enable hardware acceleration for host platform.
+        System.setProperty("sun.java2d." + getHardwareAcceleration(), "true");
 
         EditorWindow window = new EditorWindow();
         window.run();
+    }
+
+    private static String getHardwareAcceleration() {
+        String os = System.getProperty("os.name").toLowerCase();
+        if (os.contains("win")) return "d3d";
+        if (os.contains("macos")) return "metal";
+        return "opengl";
     }
 }
